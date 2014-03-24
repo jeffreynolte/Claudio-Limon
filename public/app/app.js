cdcf7a7fe1cb0dfaa4af31ba2fe527fe56ffa90b
Bernardo <bernardo@insignia4u.com>
2014-03-24 20:53:23 -0300

    Adds textAngular

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngResource', 'ngRoute']);
+angular.module('app', ['ngResource', 'ngRoute', 'textAngular']);
 
 angular.module('app').config(function ($routeProvider, $locationProvider) {
     var routeRoleChecks = {
